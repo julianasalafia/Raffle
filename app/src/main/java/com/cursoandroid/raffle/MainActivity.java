7798f5beca4ef7a3d1adef5792358fe626482499
Juliana Salafia <juliana.salafia1@gmail.com>
2022-05-17 08:54:37 -0300

    raffle game

---
+++ app/src/main/java/com/cursoandroid/raffle/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cursoandroid.raffle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void drawNumber(View view) {
+        TextView text = findViewById(R.id.selectedNumber);
+        int result = new Random().nextInt(11);
+
+        text.setText(String.valueOf(result));
+    }
+}
